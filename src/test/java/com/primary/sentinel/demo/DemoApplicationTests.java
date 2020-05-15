0e0a2ed4f936c782f6162e0aa7900538b1a1e341
liushuai <shuai9914nds@163.com>
2020-05-15 16:28:24 +0800

    第一次玩

---
+++ src/test/java/com/primary/sentinel/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primary.sentinel.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
