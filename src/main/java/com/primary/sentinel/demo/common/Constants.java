c009990beb1e501b209690364d3e4fe92e1b8354
liushuai <shuai9914nds@163.com>
2020-05-15 16:49:24 +0800

    新增公共包

---
+++ src/main/java/com/primary/sentinel/demo/common/Constants.java
@@ -0,0 +1,7 @@
+package com.primary.sentinel.demo.common;
+
+public class Constants {
+
+	public static final int PAGE_SIZE = 20;
+
+}
---
+++ src/main/java/com/primary/sentinel/demo/common/SysActionDtoGroup.java
@@ -0,0 +1,7 @@
+package com.primary.sentinel.demo.common;
+
+/**
+ * Created by Administrator on 2017/9/7.
+ */
+public interface SysActionDtoGroup {
+}
